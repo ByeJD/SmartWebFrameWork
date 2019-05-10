3dcd4131b99c2421f451651ae2179370621f8b18
15988855324@163.com <15988855324@163.com>
2019-05-11 00:36:59 +0800

    aop

--- src/main/java/com/quan/frame/common/AopHelper.java
+++ src/main/java/com/quan/frame/common/AopHelper.java
@@ -1,4 +1,77 @@
 package com.quan.frame.common;
 
+import com.quan.frame.annotation.Aspect;
+import com.quan.frame.proxy.AspectProxy;
+import com.quan.frame.proxy.Proxy;
+import com.quan.frame.proxy.ProxyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.util.*;
+
 public class AopHelper {
+    private static final Logger logger = LoggerFactory.getLogger(AopHelper.class);
+
+    static {
+        try {
+            Map<Class<?>, Set<Class<?>>> proxyMap = createProxyMap();
+            Map<Class<?>, List<Proxy>> targetMap = createTargetMap(proxyMap);
+            for (Map.Entry<Class<?>, List<Proxy>> targetEntry : targetMap.entrySet()) {
+                Class<?> targetClass = targetEntry.getKey();
+                List<Proxy> proxyList = targetEntry.getValue();
+                Object proxy = ProxyManager.createProxy(targetClass, proxyList);
+                BeanHelper.setBean(targetClass, proxy);
+            }
+        } catch (Exception e) {
+            logger.error("aop failure", e);
+        }
+    }
+
+
+    private static Set<Class<?>> createTargetClassSet(Aspect aspect) {
+        Set<Class<?>> targetClassSet = new HashSet<>();
+        Class<? extends Annotation> annatation = aspect.value();
+        if (annatation != null && !annatation.equals(Aspect.class)) {
+            targetClassSet.addAll(ClassHelper.getClassSetByAnnatation(annatation));
+        }
+
+        return targetClassSet;
+    }
+
+    private static Map<Class<?>, Set<Class<?>>> createProxyMap() {
+        Map<Class<?>, Set<Class<?>>> proxyMap = new HashMap<>();
+        Set<Class<?>> proxyClassSet = ClassHelper.getClassSetBySuper(AspectProxy.class);
+        for (Class<?> proxyClass : proxyClassSet) {
+            if (proxyClass.isAnnotationPresent(Aspect.class)) {
+                Aspect aspect = proxyClass.getAnnotation(Aspect.class);
+                Set<Class<?>> targetClassSet = createTargetClassSet(aspect);
+                proxyMap.put(proxyClass, targetClassSet);
+            }
+        }
+
+        return proxyMap;
+    }
+
+    private static Map<Class<?>, List<Proxy>> createTargetMap(Map<Class<?>, Set<Class<?>>> proxyMap) throws IllegalAccessException, InstantiationException {
+        Map<Class<?>, List<Proxy>> targetMap = new HashMap<>();
+        for (Map.Entry<Class<?>, Set<Class<?>>> proxyEntry : proxyMap.entrySet()) {
+            Class<?> proxyClass = proxyEntry.getKey();
+            Set<Class<?>> targetClassSet = proxyEntry.getValue();
+            for (Class<?> targetClass : targetClassSet) {
+                Proxy proxy = (Proxy) proxyClass.newInstance();
+                if (targetMap.containsKey(targetClass)) {
+                    targetMap.get(targetClass).add(proxy);
+                } else {
+                    List<Proxy> proxies = new ArrayList<>();
+                    proxies.add(proxy);
+                    targetMap.put(targetClass, proxies);
+                }
+            }
+        }
+
+        return targetMap;
+    }
+
+
 }
--- src/main/java/com/quan/frame/common/HelperLoader.java
+++ src/main/java/com/quan/frame/common/HelperLoader.java
@@ -1,13 +1,11 @@
 package com.quan.frame.common;
 
-import com.quan.frame.annotation.Controller;
-
 import java.util.Arrays;
 
 public class HelperLoader {
 
     public static void init(){
-        Class<?>[] classList = {ClassHelper.class,BeanHelper.class,IocHelper.class,ControllerHelper.class};
+        Class<?>[] classList = {ClassHelper.class,BeanHelper.class,AopHelper.class,IocHelper.class,ControllerHelper.class};
 
         Arrays.asList(classList).forEach((cls)->ClassUtil.loadClass(cls.getName(),true));
     }
